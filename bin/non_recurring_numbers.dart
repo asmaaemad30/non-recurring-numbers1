84e48c84984497d3e3da51697612a70579f88594
asmaaemad30 <88349587+asmaaemad30@users.noreply.github.com>
2022-12-16 01:19:53 +0200

    non-recurring-numbers
    non-recurring-numbers

---
+++ bin/non_recurring_numbers.dart
@@ -0,0 +1,12 @@
+import 'package:non_recurring_numbers/non_recurring_numbers.dart' as non_recurring_numbers;
+
+void main(List<String> arguments) {
+Set <int> numbers= {2,3,4,4,2,2,4,7,5,5,2,2,2,5,};
+numbers.forEach((element) {
+
+  print('$element Asmaa Emad');
+
+});
+
+
+}
---
+++ test/non_recurring_numbers_test.dart
@@ -0,0 +1,8 @@
+import 'package:non_recurring_numbers/non_recurring_numbers.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
